af971a0ebe7e2d780c90078f3a626c0a37b5f254
Stephen Pinkerton <stephen@pinkerton.io>
2017-02-11 15:32:35 -0800

    Fix grc commands

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,4 +1,9 @@
 # GRC colorizes nifty unix tools all over the place
+
+# FileNotFound error? https://github.com/garabik/grc/issues/51
+# ^ This should be packaged in a future grc release, but is currently only in
+# the devel branch (Feb 11, 2017). Computers, man.
+
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
   source `brew --prefix`/etc/grc.bashrc
