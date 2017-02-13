477701846256d6b3d1f2fa0ba9d37e4059534f5b
Stephen Pinkerton <stephen@pinkerton.io>
2017-02-12 22:40:42 -0800

    Fix vim and add fairyfloss theme

---
+++ iterm/install.sh
@@ -0,0 +1,2 @@
+#open -a /Applications/iTerm.app ~/.dotfiles/iterm/Solarized Dark.itermcolors
+
--- vim/install.sh
+++ vim/install.sh
@@ -1,1 +1,2 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sh '~/bin/vim -c "PluginInstall!" -c "q" -c "q"'
