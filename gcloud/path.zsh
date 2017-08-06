a1ae13d9f7fc4ef12c123302b6e47688bc67a27b
Stephen Pinkerton <stephen@pinkerton.io>
2017-08-06 13:46:45 +0100

    Add some tmux, vim, and path settings

---
+++ gcloud/path.zsh
@@ -0,0 +1,5 @@
+# Udate PATH for the Google Cloud SDK.
+if [ -f '/Users/pink/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/pink/google-cloud-sdk/path.zsh.inc'; fi
+
+# Enables shell command completion for gcloud.
+if [ -f '/Users/pink/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/pink/google-cloud-sdk/completion.zsh.inc'; fi
